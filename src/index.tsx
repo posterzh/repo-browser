2f49eb81ed4d58bd8a5284ac1bd774e7a1568ce7
Poterasu <zhdev@gmail.com>
2022-11-12 15:36:01 +0200

    set up apollo client

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,38 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {ApolloClient, ApolloProvider, createHttpLink, InMemoryCache} from "@apollo/client";
+import {setContext} from "@apollo/client/link/context";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+	document.getElementById('root') as HTMLElement
 );
+
+const httpLink = createHttpLink({
+	uri: 'https://api.github.com/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+	const token = process.env.REACT_APP_GITHUB_TOKEN;
+	return {
+		headers: {
+			...headers,
+			authorization: token ? `Bearer ${token}` : '',
+		},
+	};
+});
+
+const client = new ApolloClient({
+	link: authLink.concat(httpLink),
+	cache: new InMemoryCache(),
+});
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<ApolloProvider client={client}>
+      <App />
+		</ApolloProvider>
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
